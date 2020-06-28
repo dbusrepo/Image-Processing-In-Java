ada7d7138285d6e6262618f71d3d02f15f40fe8b
dbusato <davidedbusato@gmail.com>
2020-06-28 09:31:52 +0200

    Starting

---
+++ src/main/java/com/busatod/graphics/imageprocessing/ImageProcessing.java
@@ -0,0 +1,42 @@
+package com.busatod.graphics.imageprocessing;
+
+import com.busatod.graphics.app.GraphicsApplication;
+import com.busatod.graphics.app.Settings;
+
+public class ImageProcessing extends GraphicsApplication
+{
+	public ImageProcessing()
+	{
+		Settings settings = new Settings();
+		settings.title = "Hello Image Processing!";
+		start(settings);
+	}
+	
+	@Override
+	protected void appInit()
+	{
+	}
+	
+	@Override
+	protected void appUpdate(long elapsedTime)
+	{
+	
+	}
+	
+	@Override
+	protected void appFinishOff()
+	{
+	
+	}
+	
+	@Override
+	protected void appPrintFinalStats()
+	{
+	
+	}
+	
+	public static void main(String[] args)
+	{
+		new ImageProcessing();
+	}
+}
