e4a29e6464839eb2b3ad50b9bac5430034ef5dcb
dbusato <davidedbusato@gmail.com>
2021-01-06 14:59:24 +0100

    Squashed 'base-java-graphics/' changes from b4cbea4..f78afea
    f78afea Change method vis
    3310167 Add igraphapp interface. Refactor
    a25fcd7 Change bg color win log
    82c82da Add log support
    7169883 Add exit confirm dialog
    2cb46a9 Fix menu issues
    
    git-subtree-dir: base-java-graphics
    git-subtree-split: f78afea449fa8136906aa02df2856cd1d70cbef2

--- src/main/java/base/graphics/app/Settings.java
+++ src/main/java/base/graphics/app/Settings.java
@@ -1,27 +1,34 @@
 package base.graphics.app;
 
-public class Settings
-{
-	
-	static final String TITLE       = "Java Graphics";
-	static final int    WIDTH       = 1024;
-	static final int    HEIGHT      = 768;
-	static final int    NUM_BUFFERS = 3;
-	static final int    BIT_DEPTH   = 32;
-	static final int    TARGET_FPS  = 120;
-	
-	public String  title            = TITLE;
-	public int     width            = WIDTH;
-	public int     height           = HEIGHT;
-	public int     bitDepth         = BIT_DEPTH;
-	public boolean fullScreen       = false;
-	public int     numBuffers       = NUM_BUFFERS;
-	public int     targetFps        = TARGET_FPS;
-	public boolean showFps          = true;
+import java.util.logging.Level;
+
+public class Settings {
+
+	static final String TITLE = "Java Graphics";
+	static final int WIDTH = 1024;
+	static final int HEIGHT = 768;
+	static final int NUM_BUFFERS = 3;
+	static final int BIT_DEPTH = 32;
+	static final int TARGET_FPS = 120;
+
+	public String title = TITLE;
+	public int winWidth = WIDTH;
+	public int winHeight = HEIGHT;
+	public int bitDepth = BIT_DEPTH;
+	public boolean fullScreen = false;
+	public int numBuffers = NUM_BUFFERS;
+	public int targetFps = TARGET_FPS;
+	public boolean showFps = true;
 	public boolean showCapabilities = true;
-	
-	public void toggleFullscreen()
-	{
+	public boolean showMenu = true;
+
+	public boolean useLogWindow = true;
+	public int logWinWidth = 500;
+	public int logWinHeight = 500;
+	public boolean useLogFile = true;
+	public Level logLevel = Level.ALL;
+
+	public void toggleFullscreen() {
 		fullScreen = !fullScreen;
 	}
 }
